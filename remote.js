65d8f2f3d0efce21c799371466d23917fa5682de
Le-Vu Tran <vu.tran@employmenthero.com>
2023-04-23 10:09:56 +0700

    Add remote file to test

---
+++ remote.js
@@ -0,0 +1,49 @@
+const standardize = (voucher) => {
+  return {
+    region: "VN",
+    data: {
+      voucher_id: voucher.voucher_id,
+      top_up_amount: voucher.top_up_amount,
+      scheduled_time: Math.floor(
+        new Date(voucher.scheduled_time).getTime() / 1000.0
+      ),
+      is_rollover: true,
+    },
+  };
+};
+
+const registerVoucher = async (voucher) => {
+  return await fetch(
+    "https://admin.promotion.shopee.vn/api/gateway/v1/voucher/admin/create_quota_top_up_task",
+    {
+      method: "POST",
+      headers: {
+        accept: "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        region: "VN",
+        credentials: "include",
+      },
+      body: JSON.stringify(voucher),
+    }
+  );
+};
+
+const registerVouchers = async (vouchers) => {
+  return await Promise.all(
+    vouchers.map(async (voucher) => {
+      var standardized_voucher = standardize(voucher);
+      var response = await registerVoucher(standardized_voucher);
+      var body = response.status == 442 ? await response.text() : "";
+
+      return {
+        ...voucher,
+        request: JSON.stringify(standardized_voucher),
+        response: body,
+        status: response.status,
+      };
+    })
+  );
+};
+
+export { standardize, registerVoucher, registerVouchers };
